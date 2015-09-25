4a9dbed0fc563ac0641d7498f35b109dee91a16d
Z.H <hizhouhan@gmail.com>
2015-09-25 22:46:36 +0800

    centos

--- clfetch/main.py
+++ clfetch/main.py
@@ -4,17 +4,13 @@ from urllib.request import Request
 import urllib
 import urllib.request
 import os, sqlite3, sys
+import aiohttp
 
 RunTmp = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'RunTmp')
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'libs'))
 import database
-# dbcon = sqlite3.connect(os.path.join(RunTmp, "db.sqlite3"))
-
-a = [[1,2,], [3,4]]
-zipped = zip(a)
-for i in zipped:
-    print(i)
-
+dbcon = sqlite3.connect(os.path.join(RunTmp, "sqlite3.db"))
+database.setup('sqlite3', dbcon, False)
 q = asyncio.Queue()
 q.put_nowait(1)
 q.put_nowait(2)
@@ -26,6 +22,25 @@ q.put_nowait(2)
 
 # response = urllib.request.urlopen('http://python.org/')
 # req = Request('http://cc.ttum.pw/thread0806.php?fid=8')
-# response = urllib.request.urlopen('http://cc.ttum.pw/thread0806.php?fid=8')
-# soup = BeautifulSoup(response.read(), 'html.parser')
-# print()
+# req = urllib.request.Request(url='http://cc.ttum.pw/thread0806.php?fid=8&search=&page=2', headers=headers)
+
+# with open('a.html', 'w') as fd:
+# fd.write(urllib.request.urlopen(req).read().decode('gbk'))
+
+@asyncio.coroutine
+def fetchurl(url):
+    headers = {'User-Agent': 'Opera/9.80 (Windows NT 6.1; WOW64; U; de) Presto/2.10.289 Version/12.01',
+               'Accept-Language': 'zh-CN', 'Connection': 'Close',
+               'Accept': 'text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1'}
+    # response = yield from aiohttp.get('http://cc.ttum.pw/thread0806.php?fid=8&search=&page=2', headers=headers)
+    response = yield from aiohttp.get(url, headers=headers)
+    body = yield from response.read_and_close(decode=False)
+    soup = BeautifulSoup(body.decode('gbk'), 'html.parser')
+    tbody = soup.findAll('tbody')
+    for t in tbody:
+        for a in t.findAll('a'):
+            print(a)
+
+loop = asyncio.get_event_loop()
+q.put()
+loop.run_forever()
---
+++ python3/toolsfunc.py
@@ -0,0 +1,53 @@
+#coding = utf8
+import operator
+import functools
+from functools import partial
+import contextlib
+
+b = operator.itemgetter
+# functools.cmp_to_key()
+# s = sorted(student_objects, key=attrgetter('age'))
+int2 = partial(int, base=2)
+print (int2('1000'))
+
+class test:
+     def __enter__(self):
+          print("enter")
+          return 1
+     def __exit__(self, *args):
+          print("exit")
+
+def function():
+     print('function()')
+     return 111
+
+# with test() as t:
+#      function()
+#      print('t is', t)
+
+import time
+def foo():
+    print( 'in foo()')
+def timeit(func):
+    start = time.clock()
+    func()
+    end =time.clock()
+    print('used:', end - start)
+
+timeit(foo)
+
+
+def foo():
+    print('in foo()')
+# 定义一个计时器，传入一个，并返回另一个附加了计时功能的方法
+def timeit(func):
+    # 定义一个内嵌的包装函数，给传入的函数加上计时功能的包装
+    def wrapper():
+        start = time.clock()
+        func()
+        end =time.clock()
+        print('used:', end - start)
+    # 将包装后的函数返回
+    return wrapper
+foo = timeit(foo)
+foo()
