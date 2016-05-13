20cb860f46e8c0a3f016682a5625d3e7ff49f2ee
Z.H <hizhouhan@gmail.com>
2016-05-13 23:28:40 +0800

    centos

--- Study/testserver/wsgitest.py
+++ Study/testserver/wsgitest.py
@@ -4,7 +4,7 @@ from wsgiref.simple_server import make_server
 def simple_app(environ, start_response):
     status = '200 OK'
     for key in environ:
-        print key, environ[key]
+        print(key, environ[key])
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return [u"This is hello wsgi app".encode('utf8'), ]
@@ -12,5 +12,5 @@ def simple_app(environ, start_response):
 
 if __name__ == "__main__":
     httpd = make_server('', 8001, simple_app)
-    print "Serving on port 8000..."
+    print("Serving on port 8000...")
     httpd.serve_forever()
--- pypystudy/test.py
+++
@@ -1,4 +0,0 @@
-import greenlet
-import markupsafe
-from sae.taskqueue import TaskQueue
-import pylibmc_fake
